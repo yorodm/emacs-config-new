4d4d376f4325c42855ecefd6e7a8c97cf01a4192
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-07-09 16:40:29 +0300

    wip: before pulling changes

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -14,20 +14,20 @@
   (interactive)
   (cond
    ((find-font (font-spec :name "Cascadia Code"))
-	(set-frame-font "Cascadia Code-13")
-	(setq default-frame-alist '((font . "Cascadia Code-13"))))
+	(set-frame-font "Cascadia Code-14")
+	(setq default-frame-alist '((font . "Cascadia Code-14"))))
    ((find-font (font-spec :name "Fira Code"))
-	(set-frame-font "Fira Code-12")
-	(setq default-frame-alist '((font . "Fira Code-12"))))
+	(set-frame-font "Fira Code-14")
+	(setq default-frame-alist '((font . "Fira Code-14"))))
    ((find-font (font-spec :name "Inconsolata"))
-	(set-frame-font "inconsolata-11")
-	(setq default-frame-alist '((font . "inconsolata-11"))))
+	(set-frame-font "inconsolata-14")
+	(setq default-frame-alist '((font . "inconsolata-14"))))
    ((find-font (font-spec :name "Lucida Console"))
-	(set-frame-font "Lucida Console-11")
-	(setq default-frame-alist '((font . "Lucida Console-11"))))
+	(set-frame-font "Lucida Console-14")
+	(setq default-frame-alist '((font . "Lucida Console-14"))))
    ((find-font (font-spec :name "courier"))
-	(set-frame-font "courier-11")
-	(setq default-frame-alist '((font . "courier-11"))))))
+	(set-frame-font "courier-14")
+	(setq default-frame-alist '((font . "courier-14"))))))
 
 (add-hook 'focus-in-hook #'select-font)
 (add-hook 'server-after-make-frame-functions 'select-font)
@@ -63,6 +63,13 @@
                                        "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
                                        "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
                                        "\\\\" "://"))
+  (use-package emacs-emojify
+    :straight t
+    :bind
+    ("C-c ." . emojify-insert-emoji)
+    :config
+    (setq emojify-display-style 'unicode)
+    (setq emojify-emoji-styles '(unicode)))
   ;; Enables ligature checks globally in all buffers. You can also do it
   ;; per mode with `ligature-mode'.
   (global-ligature-mode t))
--- elisp/lang-rust.el
+++ elisp/lang-rust.el
@@ -3,5 +3,7 @@
 ;; rust-mode
 ;; https://github.com/rust-lang/rust-mode
 (use-package rustic
-    :straight t)
+  :straight t
+  :config
+  (setq lsp-inlay-hint-enable t))
 (provide 'lang-rust)
